1a5473a99fddbedc4aad4ae5396a741f454d87f9
evagolabek <golabek.eva@gmail.com>
2018-10-01 22:38:41 +0200

    store provider tag and reducer

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from  'redux';
+import reducers from './reducers';
 
+// created a store and passed it to the provider tag
+// created one reducers and passed it to the store
 const App = () => {
   return (
-    <View />
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
   );
 };
 
